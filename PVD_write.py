e4fc0839517bfe99470ca5b7cc137f4265dce45f
Alex Soong <2236325021@qq.com>
2022-04-11 09:54:30 +0800

    wrote sth

--- PVD_write.py
+++ PVD_write.py
@@ -4,6 +4,61 @@ import bitstring as bs
 import sys, os
 
 
+def zigzag(_matrix):
+    m, n = _matrix.shape
+    lst = []
+    x, y = 0, 0
+    slope = True
+    for i in range(m * n):
+        # print(x, y, i)
+        lst.append(_matrix[x][y])
+        if (x == 0 or x == m - 1) and y < n - 1 and slope == True:
+            y += 1
+            slope = False
+        elif (y == 0 or y == n - 1) and x < m - 1 and slope == True:
+            x += 1
+            slope = False
+        elif (x + y) % 2 == 0:
+            x, y = x - 1, y + 1
+            slope = True
+        else:
+            x, y = x + 1, y - 1
+            slope = True
+    return lst
+
+
+# 计算log2wk和lk
+def calcuSize(_p1, _p2):
+    ranks = [[0, 7], [8, 15], [16, 31], [32, 63], [64, 127], [128, 255]]
+    logs = [3, 3, 4, 5, 6, 7]
+    d = _p2 - _p1
+    if d < 0:
+        d = -d
+    for i in range(6):
+        if d >= ranks[i][0] and d <= ranks[i][1]:
+            return (logs[i], ranks[i][0])
+    return (0, 0)
+            
+
+def writeInPair(_p1, _p2, _b, _lk):
+    d = _p2 - _p1
+    dp = 0
+    p1, p2 = 0, 0
+    if d >= 0:
+        dp = _lk + _b
+    else:
+        dp = -(_lk + _b)
+    rf = (dp - d) // 2
+    rc = dp - d - rf
+    if d % 2 == 1:
+        p1 = _p1 - rc
+        p2 = _p2 + rf
+    else:
+        p1 = _p1 - rf
+        p2 = _p2 + rc
+    return (p1, p2)
+
+
 def write(fileName, infotoWrite, _label=""):
     im0 = Image.open(fileName)
     x = np.array(im0)
@@ -20,11 +75,29 @@ def write(fileName, infotoWrite, _label=""):
     return (infoSize, fSize)
 
 
+# def write(fileName, infotoWrite, _label=""):
+#     im0 = Image.open(fileName)
+#     x = np.array(im0)
+
+#     fSize = x.ravel().shape[0]
+#     infoSize = len(infotoWrite)
+#     length = fSize if fSize < infoSize else infoSize
+#     for i in range(length):
+#         x.ravel()[i] = LSBReplace(x.ravel()[i], int(infotoWrite[i]))
+
+#     im1 = Image.fromarray(x)
+#     im1.save(fileName[:-4] + "_written" + _label + ".bmp")
+
+#     return (infoSize, fSize)
+
+
 def getBitStr(fileName):
     f = open(fileName, "rb")
     str = f.read()
     str = bs.BitArray(str).bin
     return str
+
+
 
 # if __name__ == '__main__':
 
@@ -37,15 +110,21 @@ def getBitStr(fileName):
 #     bits = getBitStr(inputFileName1)
 #     write(inputFileName0, bits)
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
-    if len(sys.argv) > 1:
-        fileName =sys.argv[1]
-    else:
-        print("未指定文件名")
-        os._exit(0)
-    for i in range(5):
-        bits = getBitStr("m" + str(i+1) + ".zip")
-        write(fileName, bits, _label=str(i+1))
+#     if len(sys.argv) > 1:
+#         fileName =sys.argv[1]
+#     else:
+#         print("未指定文件名")
+#         os._exit(0)
+#     for i in range(5):
+#         bits = getBitStr("m" + str(i+1) + ".zip")
+#         write(fileName, bits, _label=str(i+1))
     
+
+a = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+b = np.array([[1, 2, 3],[4,5,6],[7,8,9]])
+
+print(zigzag(b))
+print(zigzag(a))
     
